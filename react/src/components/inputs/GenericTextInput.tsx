9ed2289467a8abc2633bbeea0aca64af330eebd0
Matthew Choulas <MatthewChoulas@gmail.com>
2022-09-30 22:27:09 -0400

    added simpple text input

--- react/src/components/inputs/GenericTextInput.tsx
+++ react/src/components/inputs/GenericTextInput.tsx
@@ -1,9 +1,13 @@
 function GenericTextInput() {
 
   return (
-    <h1 className="text-4xl">
-      
-    </h1>
+    
+   <div className="mx-3">
+    <label className="block text-[#344054] text-sm mb-2">
+      Username
+      </label>
+    <input className="shadow appearance-none border border-color-[#D0D5DD] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
+  </div>
   )
 }
 
