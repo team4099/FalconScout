43e3948bcf4080296b32c6f6c8b8a75ed528c9b9
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-09-30 22:28:21 -0400

    fixed indentation

--- react/src/components/inputs/GenericTextInput.tsx
+++ react/src/components/inputs/GenericTextInput.tsx
@@ -1,13 +1,12 @@
 function GenericTextInput() {
 
   return (
-    
-   <div className="mx-3">
-    <label className="block text-[#344054] text-sm mb-2">
-      Username
+    <div className="mx-3">
+      <label className="block text-[#344054] text-sm mb-2">
+        Username
       </label>
-    <input className="shadow appearance-none border border-color-[#D0D5DD] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
-  </div>
+      <input className="shadow appearance-none border border-color-[#D0D5DD] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
+    </div>
   )
 }
 
