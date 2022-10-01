1096be47774760662f9fd53142e90afe9d56b73b
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-09-30 23:06:00 -0400

    fixed more merge

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { DropdownTextInput, GenericTextInput } from "./components/inputs"
+import { GenericTextArea } from "./components/inputs"
 
 
 function App() {
-	return (
-		<div>
-			<GenericTextInput/>
-			<DropdownTextInput/>
-		</div>
-	)
+  return (
+    <div>
+      <GenericTextInput/>
+	  <DropdownTextInput/>
+      <GenericTextArea/>
+    </div>
+  )
 }
 
 export default App
--- react/src/components/inputs/GenericTextArea.tsx
+++ react/src/components/inputs/GenericTextArea.tsx
@@ -2,11 +2,9 @@ function GenericTextArea() {
 
     return (
     
-		<div className="mx-3">
-			<label className="block text-[#344054] text-sm mb-2">
-				Username
-			</label>
-			<input className="shadow appearance-none border border-color-[#D0D5DD] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
+		<div className="mx-3 my-3">
+			<label className="block mb-2 text-sm text-gray-[#344054]">Your message</label>
+			<textarea id="message" className="block p-4 w-full h-40 text text-[#000000] rounded-lg border border-color-[#D0D5DD] focus:outline-none" placeholder="Your message..."></textarea>
 		</div>
 
   	)
