ada01eccf75e5db30288e0b011d63f688b071761
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-09-30 22:01:14 -0400

    added file structure

---
+++ react/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { GenericTextInput } from "./components/inputs"
+
+
+function App() {
+  return (
+    <GenericTextInput/>
+  )
+}
+
+export default App
