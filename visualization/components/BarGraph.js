ea59cb5c5b51543dca412cb2f1185c7a768d9a8c
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-10-08 21:36:22 -0400

    make update not refresh graph

--- visualization/components/BarGraph.js
+++ visualization/components/BarGraph.js
@@ -1,7 +1,7 @@
 import { Graph } from "./Graph"
 
 class BarGraph {
-    constructor(id, plotOptions, dataOptions){
+    constructor(id, plotOptions, dataOptions) {
         this.uuid = Math.random().toString(36).substr(2, 9)
 
         this.container = document.getElementById(id)
@@ -9,12 +9,12 @@ class BarGraph {
         this.container.setAttribute("data-modal-toggle", this.uuid)
 
         var self = this
-        this.container.addEventListener("click", function(){
+        this.container.addEventListener("click", function () {
             self.setupEdit()
             document.getElementById('fakeToggle').click()
         })
 
-        
+
         this.formula = dataOptions.formula
 
         this.selectedColumnOptions = dataOptions.selectedOptions
@@ -35,10 +35,10 @@ class BarGraph {
 
     }
 
-    generateData(){
+    generateData() {
         this.seriesOptions = []
 
-        for (const selected of this.selectedColumnOptions){
+        for (const selected of this.selectedColumnOptions) {
             this.seriesOptions.push(
                 {
                     x: selected.toString(),
@@ -48,20 +48,20 @@ class BarGraph {
         }
     }
 
-    setupEdit(){
+    setupEdit() {
         var formString = ``
 
         var self = this
-        document.getElementById("getEditedData").addEventListener("click", function(){
+        document.getElementById("getEditedData").addEventListener("click", function () {
             self.pushEdit()
         })
 
-        for (const i of this.allColumnOptions){
-            if (this.selectedColumnOptions.includes(i)){
+        for (const i of this.allColumnOptions) {
+            if (this.selectedColumnOptions.includes(i)) {
                 formString += `
                 <div class="flex items-center">
                     <input checked id="${i}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label for="${i}" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">${i}</label>
+                    <label id="for="${i}" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">${i}</label>
                 </div>
                 `
             }
@@ -76,13 +76,13 @@ class BarGraph {
         }
 
         document.getElementById("editableFormContainer").innerHTML = formString
-        
+
     }
 
     pushEdit() {
         this.selectedColumnOptions = []
-        for (const i of this.allColumnOptions){
-            if (document.getElementById(i.toString()).checked){
+        for (const i of this.allColumnOptions) {
+            if (document.getElementById(i.toString()).checked) {
                 this.selectedColumnOptions.push(i)
             }
         }
