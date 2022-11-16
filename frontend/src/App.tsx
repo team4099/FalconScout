b344dac82ebc3d565dc3c19c2f4bc32fd9237a57
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-16 07:41:26 -0500

    Added all demos to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { DropdownTextInput, GenericTextInput, SliderInput, GenericTextArea, IncrementNumberInput } from "./components/inputs"
 import { DarkButton } from "./components/buttons"
-import { GenericDropdown, GenericMultiDropdown, GenericToggle } from './components/selects'
+import { GenericDropdown, GenericMultiDropdown, GenericToggle, GenericRadioSelect, GenericCheckboxSelect } from './components/selects'
+import { GenericHeaderOne, GenericHeaderTwo } from './components/text'
+
 
 
 function App() {
   return (
     <div>
+      <div>
+        <GenericHeaderOne text="Header One"/>
+        <GenericHeaderTwo text="Header Two"/>
+      </div>
       <div>
         <GenericTextInput text="Username" placeholder={["pranav"]}/>
         <DropdownTextInput text="Match" options={["qm", "qf", "sf", "f"]} placeholder={["10"]} route="todo"/>
@@ -20,6 +26,8 @@ function App() {
         <GenericDropdown text="Zones" options={["tarmac", "fender", "hangar", "terminal"]}/>
         <GenericMultiDropdown text="Zones" options={["tarmac", "fender", "hangar", "terminal"]}/>
         <GenericToggle text="Taxied"/>
+        <GenericRadioSelect text="Zones" options={["tarmac", "fender", "hangar", "terminal"]}/>
+        <GenericCheckboxSelect text="Zones" options={["tarmac", "fender", "hangar", "terminal"]}/>
       </div>
       <div>
         <DarkButton text="Submit" route="todo"/>
