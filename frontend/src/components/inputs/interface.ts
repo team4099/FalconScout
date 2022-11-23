61f8d04d3aaea73dd1d94bb49f3b26e47e6e9a22
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-23 18:40:40 -0500

    remove unnecessary interface which is done elsewhere

--- frontend/src/components/inputs/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface TextSetup {
-    text: string;
-    placeholder: Array<string>;
-    route: string;
-}
-
-export interface DropDownSetup extends TextSetup {
-    options: Array<string>
-}
