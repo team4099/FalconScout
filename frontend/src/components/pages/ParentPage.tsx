148a7841757e9502d57d5f3a3ee03f1060589e1a
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-12-30 00:05:05 -0500

    pwa hellscape

--- frontend/src/components/pages/ParentPage.tsx
+++ frontend/src/components/pages/ParentPage.tsx
@@ -14,7 +14,7 @@ export function ParentPage(props: ParentPageSetup){
                 props?.pageSetup?.map((page: any) => {
                     return (
                         <Link to={'/'+page.name.replaceAll(/\s/g,'')} className="text-2xl">
-                            <div className="mb-4 w-fullblock max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
+                            <div className="mb-4 w-full block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
                                 <h1 className="text-2xl font-bold mb-4">{page.name}</h1>
                                 <h1 className="text-lg font-regular mb-2">{page.description}</h1>
                             </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { HashRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 <HashRouter>
     <App />
   </HashRouter>
 )
+
+registerServiceWorker();
