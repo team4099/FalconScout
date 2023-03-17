a23cc2a7b8e1b3b2f2126eeaca3ef3b961247853
Matthew Choulas <MatthewChoulas@gmail.com>
2023-03-16 21:15:33 -0400

    made qm default match key

--- scoutingapp/src/components/inputs/DropdownTextInput.tsx
+++ scoutingapp/src/components/inputs/DropdownTextInput.tsx
@@ -24,7 +24,6 @@ function DropdownTextInput(props: ComponentSetup) {
             </label>
             <div className="flex appearance-none h-[40px] border border-color-[#D0D5DD] border-[1.5px] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 <select name="option" id="options" className="inline" onChange={updateStateDropdown}>
-                    <option value="none">none</option>
                     { props.options?.map (option => (
                         <option value={option}>{option}</option>
                     ))}
