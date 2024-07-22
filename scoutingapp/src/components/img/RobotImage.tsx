3c46c8abe3cf8eda803fb153e96ed6dd5b24cfb7
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-07-22 16:54:06 -0400

    get robot number properly (doesn't reload)

--- scoutingapp/src/components/img/RobotImage.tsx
+++ scoutingapp/src/components/img/RobotImage.tsx
@@ -1,20 +1,18 @@
 import React from "react"
 import { ComponentSetup } from "../interface"
 
-interface RobotImageProps extends ComponentSetup {
-  robotNumber?: string;
-}
-
 export const emptyImage = <img
   style={{width: "80%", height: "80%", alignItems: 'center', justifyContent: 'center'}}
   src='../img/gray.png'
   alt={`Picture of robot`}
 />
 
-export function RobotImage({ robotNumber = "4099", ...props }: RobotImageProps) {
+export function RobotImage(props: ComponentSetup) {
+  const robotNumber = props.getValue["TeamNumber"]
+
   return (
     <img
-      style={{ width: "60%", height: "60%", alignItems: 'center', justifyContent: 'center', margin: "auto"}}
+      style={{ width: "60%", height: "60%", alignItems: 'center', justifyContent: 'center', margin: "auto" }}
       src={`./src/components/img/${robotNumber}.png`}
       alt={`Picture of robot ${robotNumber}`}
     />
