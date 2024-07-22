5fad03174180045c08a9d9312b9f2551e9f01025
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-07-22 14:17:49 -0400

    create empty image property

--- scoutingapp/src/components/img/RobotImage.tsx
+++ scoutingapp/src/components/img/RobotImage.tsx
@@ -5,6 +5,12 @@ interface RobotImageProps extends ComponentSetup {
   robotNumber?: string;
 }
 
+export const emptyImage = <img
+  style={{width: "80%", height: "80%", alignItems: 'center', justifyContent: 'center'}}
+  src='../img/gray.png'
+  alt={`Picture of robot`}
+/>
+
 export function RobotImage({ robotNumber = "4099", ...props }: RobotImageProps) {
   return (
     <img
