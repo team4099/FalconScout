fa63beefa8aececb9d8be90d163c763b9c74d95c
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-07-24 19:21:51 -0400

    redesign get images to work from tba, accepts imgur and insta

--- scoutingapp/src/getImagesTBA.py
+++ scoutingapp/src/getImagesTBA.py
@@ -2,17 +2,28 @@ import os
 import time
 import requests
 
-TBA_API_KEY = r'qtKgj5EgNArGaDykXISRkNepxhMuQbyEcVkwu2kBNUxtbiSBxKzpgStr0gIXasJP'
-EVENT_KEY = 'cur2024'
+TBA_API_KEY = r''
+IMGUR_CLIENT_ID = r''
+IMGUR_ETAG = r''
+EVENT_KEY = r''
 
-if not (TBA_API_KEY and EVENT_KEY):
+if not (TBA_API_KEY and IMGUR_CLIENT_ID and IMGUR_ETAG and EVENT_KEY):
     raise PermissionError("Some keys are missing! Go into code and insert your keys.")
 
-FOLDER_PATH = './components/img'
+FOLDER_PATH = './src/components/img'
 
 # Headers for the API request
 TBA_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+    'Referer': 'https://www.thebluealliance.com/',
     'X-TBA-Auth-Key': TBA_API_KEY
+}
+
+IMGUR_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+    'Referer': 'https://imgur.com/',
+    'Authorization': f"Client-ID {IMGUR_CLIENT_ID}",
+    "ETag": IMGUR_ETAG,
 }
 
 
@@ -30,7 +41,7 @@ def fetch_teams(event_key) -> list[int]:
     return sorted([team['team_number'] for team in response.json()])
 
 
-def scrape_robot_image(team_number: int):
+def scrape_robot_image(team_number: int) -> str:
     """
     Returns the url of the location of the robot image used on TBA.
     This function fails (raises TypeError or returns None) when there is no robot image found on TBA.
@@ -39,21 +50,48 @@ def scrape_robot_image(team_number: int):
     :returns: The url of the image
     """
 
-    url = f"https://www.thebluealliance.com/team/{team_number}"
+    url = f'https://www.thebluealliance.com/api/v3/team/frc{team_number}/media/2024'
+    response = requests.get(url, headers=TBA_HEADERS)
+    response.raise_for_status()
+    data = [x for x in response.json() if x['type'] not in ['avatar', 'youtube']]
+    return data[0]['direct_url']
 
 
-
-def download_image(team_number: int, image_url: str) -> None:
+def download_image(team_number: int, image_url: str, image_host: str) -> None:
     """
     Downloads the image from the given url to the appropirate place in code.
     Will download to scoutingapp/src/components/img and will download as team_number.jpeg.
 
     :param team_number: The team number for the robot
     :param image_url: The url location of the image that needs to be downloaded
+    :param image_host: Website hosting image (instagram | imgur)
     :returns: None
     """
 
+    if image_host == "imgur":
+        image_hash = image_url.split("/")[3].removesuffix(".jpg").removesuffix(".jpeg").removesuffix(".png")
+        image_ending = image_url.split(".")[-1]
+        image_ending = "jpeg" if image_ending == "jpg" else image_ending
 
+        image_url = f'https://i.imgur.com/{image_hash}.{image_ending}'
+
+        image_response = requests.get(image_url, headers=IMGUR_HEADERS)
+    elif image_host == "instagram":
+        image_url = f'https://thebluealliance.com/{image_url}'
+        image_ending = "jpeg"
+
+        image_response = requests.get(image_url, headers=TBA_HEADERS)
+    else:
+        return
+
+    image_response.raise_for_status()
+
+    file_path = os.path.join(FOLDER_PATH, f"{team_number}.{image_ending}")
+
+    with open(file_path, 'wb') as f:
+        f.write(image_response.content)
+
+    return
 
 
 def main(teams: tuple[int] = ()) -> None:
@@ -73,7 +111,7 @@ def main(teams: tuple[int] = ()) -> None:
     failed_teams = {}
     for team in teams:
         print(f'{team}: Starting...')
-        time.sleep(0.5)
+        time.sleep(2)
 
         try:
             url = scrape_robot_image(team)
@@ -83,14 +121,16 @@ def main(teams: tuple[int] = ()) -> None:
                 print(f"{team}: Failed to retrieve image link.")
                 failed_teams[team] = "Failed to retrieve image link."
                 continue
-            if "imgur" not in url:
-                print(f"{team}: Image is not imgur.")
-                failed_teams[team] = "Image is not imgur."
-                continue
+            if "imgur" in url:
+                download_image(team, url, "imgur")
+            elif "instagram" in url:
+                download_image(team, url, "instagram")
+            else:
+                print(f"{team}: Image host not recognized.")
+                failed_teams[team] = "Image host not recognized."
 
-            download_image(team, url)
             print(f'{team}: Success.')
-        except TypeError:
+        except (TypeError, IndexError):
             print(f"{team}: Image not found.")
             failed_teams[team] = "Image not found."
         except Exception as e:
@@ -103,14 +143,10 @@ def main(teams: tuple[int] = ()) -> None:
     print("NOTE: Some teams did not run properly.")
     print(*failed_teams.items(), sep="\n")
 
-    with open("image_errors.txt", 'w') as err_file:
+    with open("./src/image_errors.txt", 'w') as err_file:
         for fail in failed_teams.items():
             err_file.write(str(fail) + "\n")
 
 
 if __name__ == '__main__':
-    url = f'https://www.thebluealliance.com/api/v3/team/frc4099/media/2024'
-    response = requests.get(url, headers=TBA_HEADERS)
-    response.raise_for_status()
-    print(response.json())
-
+    main()
